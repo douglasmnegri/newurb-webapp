b0a9eda59b63fd71ca0c0e69e04475a8467084f2
douglasmnegri <douglasmnegri@gmail.com>
2025-10-18 16:23:06 -0300

    feat: add the URL for each of the social media

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { AiOutlineFacebook, AiOutlineX, AiOutlineGithub, AiOutlineLinkedin, AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineFacebook, AiOutlineX, AiOutlineGithub, AiOutlineLinkedin, AiOutlineInstagram, AiOutlineYoutube } from "react-icons/ai";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -6,22 +6,16 @@ const Footer = () => {
   return (
     <footer className="bg-white border-t border-gray-100 dark:bg-black shadow-sm text-gray-400 py-6 dark:border-t dark:border-gray-800">
       <div className="container mx-auto px-4 flex flex-wrap justify-center sm:justify-between items-center text-sm">
-        <p className="ml-4">&copy; {currentYear} Your Company Name. All rights reserved.</p>
+        <p className="ml-4">&copy; {currentYear} New Urb. All rights reserved.</p>
         <div className="flex space-x-4 mt-2 mr-4 sm:mt-0">
-          <a href="#" aria-label="Facebook" className="hover:text-gray-300">
+          <a href="https://www.facebook.com/profile.php?id=61565821023778" aria-label="Facebook" className="hover:text-gray-300">
             <AiOutlineFacebook className="w-5 h-5" />
           </a>
-          <a href="#" aria-label="LinkedIn" className="hover:text-gray-300">
-            <AiOutlineLinkedin className="w-5 h-5" />
-          </a>
-          <a href="#" aria-label="Instagram" className="hover:text-gray-300">
+          <a href="https://www.instagram.com/new.urb" aria-label="Instagram" className="hover:text-gray-300">
             <AiOutlineInstagram className="w-5 h-5" />
           </a>
-          <a href="#" aria-label="X (formerly Twitter)" className="hover:text-gray-300">
-            <AiOutlineX className="w-5 h-5" />
-          </a>
-          <a href="#" aria-label="GitHub" className="hover:text-gray-300">
-            <AiOutlineGithub className="w-5 h-5" />
+          <a href="#" aria-label="Youtube" className="hover:text-gray-300">
+            <AiOutlineYoutube className="w-5 h-5" />
           </a>
         </div>
       </div>
