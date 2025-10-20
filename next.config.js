706e6f7002cf34541f8abb2debe84789d8adf5f7
douglasmnegri <douglasmnegri@gmail.com>
2025-10-20 20:19:52 -0300

    fix: remove trailing slash from assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  // Only use these settings for production (GitHub Pages)
   ...(isProd && {
     output: 'export',
     basePath: '/newurb-webapp',
-    assetPrefix: '/newurb-webapp/',
+    assetPrefix: '/newurb-webapp',
   }),
   images: { 
     unoptimized: true 
