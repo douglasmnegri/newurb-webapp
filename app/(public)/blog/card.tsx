d453862739e50f99329095dfdd1525aac08b10ff
douglasmnegri <douglasmnegri@gmail.com>
2025-10-20 19:39:56 -0300

    fix: remove image from the blog cards

--- app/(public)/blog/card.tsx
+++ app/(public)/blog/card.tsx
@@ -1,25 +1,29 @@
 export interface CardProps {
   title: string;
-  date: string
+  date: string;
   description: string;
-  imageUrl: string;
   link: string;
 }
 
-function Card({ title, date, description, imageUrl, link }: CardProps) {
+function Card({ title, date, description, link }: CardProps) {
   return (
-    <a href={link} className="block border-t-2 border-gray-300 dark:border-gray-800  overflow-hidden shadow-sm hover:shadow-md transition-shadow duration-300">
+    <a
+      href={link}
+      className="block border-t-2 border-gray-300 dark:border-gray-800  overflow-hidden shadow-sm hover:shadow-md transition-shadow duration-300"
+    >
       <div className="flex flex-col md:flex-row gap-4 m-4 p-4">
         <div className="flex items-center">
           <p className="text-gray-500 text-opacity-75">{date}</p>
         </div>
         <div className="p-4">
-          <h3 className="text-lg font-semibold mb-2 text-gray-900 dark:text-white">{title}</h3>
+          <h3 className="text-lg font-semibold mb-2 text-gray-900 dark:text-white">
+            {title}
+          </h3>
           <p className="text-gray-600 dark:text-gray-300">{description}</p>
         </div>
       </div>
     </a>
   );
-}  
+}
 
 export default Card;
--- app/(public)/blog/page.tsx
+++ app/(public)/blog/page.tsx
@@ -1,10 +1,9 @@
 import Card from "./card";
-import agent from "@/public/images/agent.webp";
 
 export default function BlogPage() {
   return (
     <main className="m-12 bgray-50 dark:bg-gray-900">
-      <div className="space-y-2 pb-8 pt-6 md:space-y-5 bg-white dark:bg-gray-900"> 
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5 bg-white dark:bg-gray-900">
         <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           Latest news
         </h1>
@@ -17,21 +16,18 @@ export default function BlogPage() {
           title="Sample Post"
           date="January 1, 2024"
           description="This is a sample blog post description."
-          imageUrl={agent.src}
           link="/blog/sample-post"
         />
         <Card
           title="Sample Post"
           date="February 4, 2024"
           description="This is a sample blog post description."
-          imageUrl={agent.src}
           link="/blog/sample-post"
         />
         <Card
           title="Sample Post"
           date="March 3, 2024"
           description="This is a sample blog post description."
-          imageUrl={agent.src}
           link="/blog/sample-post"
         />
       </div>
